bba774c87e7acfe52dbee64b2dbc021222a9f8e4
MatheusBerwaldt <mathsmachado18@gmail.com>
2024-10-01 13:16:18 -0300

    configure dependecies

---
+++ src/main/java/com/SARC/SARC/SarcApplication.java
@@ -0,0 +1,14 @@
+package com.SARC.SARC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarcApplication.class, args);
+	}
+
+}
+git 
